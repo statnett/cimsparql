6c4b9cfc20baf41b43199e2210cd9f4b2cdabd24
Leif Warland <Leif.Warland@statnett.no>
2019-12-19 10:23:32 +0100

    Remove unnecessary try-except clause

--- cimsparql/model.py
+++ cimsparql/model.py
@@ -208,10 +208,7 @@ class CimModel(Prefix):
                 result.loc[:, column] = result.loc[:, column].str.contains("True|true")
             else:
                 result.loc[result[column] == "None", column] = ""
-                try:
-                    result.loc[:, column] = pd.to_numeric(result[column]).astype(column_type)
-                except ValueError:
-                    raise
+                result.loc[:, column] = pd.to_numeric(result[column]).astype(column_type)
 
     @classmethod
     def col_map(cls, data_row, columns) -> Tuple[Dict[str, str]]:
