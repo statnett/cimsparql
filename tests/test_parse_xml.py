8d6c936bcf10e374a4f54875f383c9f1cb822f40
Leif Warland <leif.warland@statnett.no>
2021-03-09 17:53:29 +0100

    Fix issues raised by sonarqube

--- tests/test_parse_xml.py
+++ tests/test_parse_xml.py
@@ -1,6 +1,8 @@
 import os
 import tempfile
+from itertools import product
 from pathlib import Path
+from typing import List
 
 import pandas as pd
 import pendulum
@@ -12,9 +14,21 @@ from cimsparql.parse_xml import CimXmlStr, SvTpCimXml
 root = Path(__file__).parent.parent
 
 
+def cim_file(date: pendulum.DateTime, profile: str) -> str:
+    return f"cim_{date.format('YYYYMMDD_HHmmss')}_mis_bymin_rtnet_ems_{profile}.xml"
+
+
+tz = "Europe/Oslo"
+
+
 @pytest.fixture(scope="module")
-def sv_tp_cim() -> SvTpCimXml:
-    paths = [root / "data" / f"{profile}.xml" for profile in ["sv", "tp"]]
+def profiles():
+    return ["sv", "tp"]
+
+
+@pytest.fixture(scope="module")
+def sv_tp_cim(profiles: List[str]) -> SvTpCimXml:
+    paths = [root / "data" / f"{profile}.xml" for profile in profiles]
     return SvTpCimXml(*paths)
 
 
@@ -66,9 +80,9 @@ def test_parse_sv_tp_cim_xml_tap_step(sv_tp_cim: SvTpCimXml):
 
 def test_parse_cim_file():
     target_file_type = "sv"
-    target_date = pendulum.datetime(2019, 11, 2, 0, 13, 40, tz="Europe/Oslo")
+    target_date = pendulum.datetime(2019, 11, 2, 0, 13, 40, tz=tz)
 
-    file_name = "cim_20191102_001340_mis_bymin_rtnet_ems_sv.xml"
+    file_name = cim_file(target_date, "sv")
     date, file_type = parse_xml.parse_cim_file(Path(file_name).stem)
 
     assert date == target_date
@@ -76,22 +90,10 @@ def test_parse_cim_file():
 
 
 def test_find_min():
-    dates = [
-        pendulum.datetime(2019, 11, 1, 1),
-        pendulum.datetime(2019, 11, 1, 2),
-        pendulum.datetime(2019, 11, 1, 3),
-        pendulum.datetime(2019, 11, 1, 4),
-        pendulum.datetime(2019, 11, 1, 5),
-        pendulum.datetime(2019, 11, 1, 6),
-    ]
+    dates = [pendulum.datetime(2019, 11, 1, hour) for hour in range(1, 7)]
     date = pendulum.datetime(2019, 11, 1, 3, 23)
 
-    target_dates = [
-        pendulum.datetime(2019, 11, 1, 3),
-        pendulum.datetime(2019, 11, 1, 4),
-        pendulum.datetime(2019, 11, 1, 5),
-        pendulum.datetime(2019, 11, 1, 6),
-    ]
+    target_dates = [pendulum.datetime(2019, 11, 1, hour) for hour in [3, 4, 5, 6]]
     target_date = pendulum.datetime(2019, 11, 1, 3)
 
     result_date, result_dates = parse_xml.find_min(date, dates)
@@ -100,111 +102,81 @@ def test_find_min():
     assert target_date == result_date
 
 
-def get_file_data(path: Path):
-    files = [
-        "cim_20191102_001340_mis_bymin_rtnet_ems_sv.xml",
-        "cim_20191102_001340_mis_bymin_rtnet_ems_tp.xml",
-        "cim_20191102_001350_mis_bymin_rtnet_ems_sv.xml",
-        "cim_20191102_001350_mis_bymin_rtnet_ems_tp.xml",
-    ]
-    date_a = pendulum.datetime(2019, 11, 2, 0, 13, 40, tz="Europe/Oslo")
-    date_b = pendulum.datetime(2019, 11, 2, 0, 13, 50, tz="Europe/Oslo")
-
+def get_file_data(path: Path, profiles: List[str]):
+    dates = [pendulum.datetime(2019, 11, 2, 0, 13, seconds, tz=tz) for seconds in [40, 50]]
+    files = [cim_file(date, profile) for date, profile in product(dates, profiles)]
     file_dict = {
-        date_a: {"sv_path": path / files[0], "tp_path": path / files[1]},
-        date_b: {"sv_path": path / files[2], "tp_path": path / files[3]},
+        date: {f"{profile}_path": path / cim_file(date, profile) for profile in profiles}
+        for date in dates
     }
-
     return file_dict, files
 
 
-def test_get_files():
-
+def test_get_files(profiles: List[str]):
     with tempfile.TemporaryDirectory() as tmp_dir:
-        target_d, files = get_file_data(Path("tmp") / tmp_dir)
-        [open(os.path.join(tmp_dir, f), "w") for f in files]
-        file_d = dict(parse_xml.get_files(Path(tmp_dir)))
+        target_d, files = get_file_data(Path("tmp") / tmp_dir, profiles)
+        [open(os.path.join(tmp_dir, file), "w") for file in files]
+        file_d = parse_xml.get_files(Path(tmp_dir))
 
     assert target_d == file_d
 
 
-def test_get_sv_tp():
+def test_get_sv_tp(profiles: List[str]):
+    file_date = pendulum.datetime(2019, 11, 2, 0, 13, 40, tz=tz)
     date_path = Path("191102")
-    target = {
-        "sv_path": date_path / "cim_20191102_001340_mis_bymin_rtnet_ems_sv.xml",
-        "tp_path": date_path / "cim_20191102_001340_mis_bymin_rtnet_ems_tp.xml",
-    }
-    file_dict, _ = get_file_data(date_path)
+    target = {f"{profile}_path": date_path / cim_file(file_date, profile) for profile in profiles}
+    file_dict, _ = get_file_data(date_path, profiles)
 
-    date = pendulum.datetime(2019, 11, 2, 0, 12, 42, tz="Europe/Oslo")
+    date = pendulum.datetime(2019, 11, 2, 0, 12, 42, tz=tz)
     sv_tp, f_c = parse_xml.get_sv_tp(date, Path(""), file_dict)
     assert sv_tp == target
 
 
-def test_get_cim_files():
-
-    dates = [
-        pendulum.datetime(2019, 11, 2, 0, 13, 45, tz="Europe/Oslo"),
-        pendulum.datetime(2019, 11, 2, 0, 13, 55, tz="Europe/Oslo"),
-    ]
+def test_get_cim_files(profiles: List[str]):
+    dates = [pendulum.datetime(2019, 11, 2, 0, 13, seconds, tz=tz) for seconds in [45, 55]]
 
     with tempfile.TemporaryDirectory() as tmp_dir:
         root_path = Path("tmp") / tmp_dir
         file_path = Path("2019") / "191102"
         target_path = root_path / file_path
-        target_d, files = get_file_data(file_path)
-        os.makedirs(os.path.join(root_path, file_path))
-
-        [open(target_path / f, "w") for f in files]
+        _, files = get_file_data(file_path, profiles)
+        (root_path / file_path).mkdir(parents=True)
+        [(target_path / file).open("w") for file in files]
         result = parse_xml.get_cim_files(root_path, dates)
 
     target = {
-        dates[0]: {
-            "sv_path": target_path / "cim_20191102_001350_mis_bymin_rtnet_ems_sv.xml",
-            "tp_path": target_path / "cim_20191102_001350_mis_bymin_rtnet_ems_tp.xml",
-        },
-        dates[1]: {
-            "sv_path": target_path / "cim_20191102_001350_mis_bymin_rtnet_ems_sv.xml",
-            "tp_path": target_path / "cim_20191102_001350_mis_bymin_rtnet_ems_tp.xml",
-        },
+        date: {
+            f"{profile}_path": target_path / cim_file(dates[-1].subtract(seconds=5), profile)
+            for profile in profiles
+        }
+        for date in dates
     }
+
     assert result == target
 
 
-def test_get_cim_files_no_o_disc():
-
-    dates = [
-        pendulum.datetime(2019, 11, 2, 0, 13, 44, tz="Europe/Oslo"),
-        pendulum.datetime(2019, 11, 2, 0, 13, 55, tz="Europe/Oslo"),
-    ]
+def test_get_cim_files_no_o_disc(profiles: List[str]):
+    dates = [pendulum.datetime(2019, 11, 2, 0, 13, seconds, tz=tz) for seconds in [44, 55]]
+    file_dates = [date.subtract(seconds=seconds) for date, seconds in zip(dates, [4, 5])]
 
     with tempfile.TemporaryDirectory() as tmp_dir:
         root_path = Path("tmp") / tmp_dir
 
-        file_b_path = Path("2019") / "file_b_path"
-        file_a_path = Path("file_a_path")
+        file_paths = [Path("file_a_path"), Path("2019") / "file_b_path"]
 
-        target_a_path = root_path / file_a_path
-        target_b_path = root_path / file_b_path
+        _, files = get_file_data(Path(""), profiles)
 
-        target_d, files = get_file_data(Path(""))
-
-        os.makedirs(os.path.join(root_path, target_a_path))
-        os.makedirs(os.path.join(root_path, target_b_path))
-
-        [open(target_a_path / f, "w") for f in files[:2]]
-        [open(target_b_path / f, "w") for f in files[2:]]
+        for file_path, target_files in zip(file_paths, [files[:2], files[2:]]):
+            (root_path / file_path).mkdir(parents=True)
+            [(root_path / file_path / file).open("w") for file in target_files]
 
         result = parse_xml.get_cim_files(root_path, dates)
 
     target = {
-        dates[0]: {
-            "sv_path": target_a_path / "cim_20191102_001340_mis_bymin_rtnet_ems_sv.xml",
-            "tp_path": target_a_path / "cim_20191102_001340_mis_bymin_rtnet_ems_tp.xml",
-        },
-        dates[1]: {
-            "sv_path": target_b_path / "cim_20191102_001350_mis_bymin_rtnet_ems_sv.xml",
-            "tp_path": target_b_path / "cim_20191102_001350_mis_bymin_rtnet_ems_tp.xml",
-        },
+        date: {
+            f"{profile}_path": root_path / target_path / cim_file(file_date, profile)
+            for profile in profiles
+        }
+        for date, file_date, target_path in zip(dates, file_dates, file_paths)
     }
     assert result == target
