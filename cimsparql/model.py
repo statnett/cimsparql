3c64ed4d74c28773d08cb2d3a4ee7af5e1c5e137
Leif Warland <leif.warland@statnett.no>
2022-09-15 07:32:03 +0200

    Remove delta_power from bus_data

--- cimsparql/model.py
+++ cimsparql/model.py
@@ -133,7 +133,6 @@ class CimModel(Model):
         with_market: bool = True,
         with_dummy_buses: bool = False,
         container: bool = False,
-        delta_power: bool = True,
         network_analysis: bool = True,
         limit: Optional[int] = None,
         dry_run: bool = False,
@@ -147,17 +146,13 @@ class CimModel(Model):
            dry_run: return string with sql query
         """
         container_variable = "?container" if container else ""
-        query = queries.bus_data(
-            region, sub_region, with_market, container_variable, self.cim_version, delta_power
-        )
+        query = queries.bus_data(region, sub_region, with_market, container_variable)
         if with_dummy_buses:
             dummy_bus_query = queries.three_winding_dummy_bus(
-                region, sub_region, with_market, container_variable, network_analysis, delta_power
+                region, sub_region, with_market, container_variable, network_analysis
             )
             combined = sup.combine_statements(query, dummy_bus_query, split=union_split)
             variables = ["?node", "?name", "?busname", "?un", "?station"]
-            if delta_power:
-                variables.append("?delta_p")
             if with_market:
                 variables.append("?bidzone")
             if container:
@@ -445,7 +440,7 @@ class CimModel(Model):
             self.cim_version,
         )
         if dry_run:
-            return self.client.query_with_header(query)
+            return self.client.query_with_header(query, True, limit)
         return self.get_table_and_convert(query, limit, index="mrid")
 
     def transformers_connected_to_converter(
@@ -732,14 +727,6 @@ class CimModel(Model):
         if dry_run:
             return self.client.query_with_header(query, limit)
         return self.get_table_and_convert(query, limit)
-
-    def delta_node_power(
-        self, node: str = "node", limit: Optional[int] = None, dry_run: bool = False
-    ):
-        query = queries.node_delta_power(node, self.cim_version, "?delta_p")
-        if dry_run:
-            return self.client.query_with_header(query, limit)
-        return self.get_table_and_convert(query, limit, index="mrid")
 
     def disconnected(
         self, index: Optional[str] = None, limit: Optional[int] = None, dry_run: bool = False
