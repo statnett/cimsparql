142a4863813910c24d3fe209309f003dcad235e5
Leif Warland <leif.warland@statnett.no>
2022-09-15 08:18:55 +0200

    Remove branch_loss and update enums

--- cimsparql/sv_queries.py
+++ cimsparql/sv_queries.py
@@ -1,9 +1,7 @@
-from typing import Iterable, List, Union
+from typing import Iterable, List, Optional, Union
 
-from cimsparql.cim import ID_OBJ, TC_EQUIPMENT, TR_END, TR_WINDING, LineTypes
-from cimsparql.enums import Power, SvPowerFlow, SvStatus, SvTapStep, Voltage
+from cimsparql.enums import Power, SvPowerFlow, SvStatus, SvTapStep, SvVoltage, Voltage
 from cimsparql.query_support import (
-    acdc_terminal,
     combine_statements,
     common_subject,
     group_query,
@@ -11,46 +9,29 @@ from cimsparql.query_support import (
 )
 
 
-def _query_str(var_list: Iterable[Union[Voltage, Power]], rdf_type: str, connection: str) -> str:
+def _query_str(
+    var_list: Iterable[Union[Voltage, Power]],
+    rdf_type: Union[SvVoltage, SvPowerFlow],
+    connection: str,
+) -> str:
     variables = ["?mrid", *[f"?{x}" for x in var_list]]
     where = common_subject(
-        "?s",
+        "?_s",
         [
-            f"rdf:type cim:{rdf_type}",
-            f"cim:{rdf_type}.{connection} ?mrid",
-            *[f"cim:{rdf_type}.{x} ?{x}" for x in var_list],
+            f"rdf:type {rdf_type.pred()}",
+            f"{rdf_type[connection]} ?mrid",
+            *[f"{rdf_type[x]} ?{x}" for x in var_list],
         ],
     )
     return combine_statements(select_statement(variables), group_query([where]))
 
 
 def powerflow(power: Iterable[Power] = Power) -> str:
-    return _query_str(power, "SvPowerFlow", "Terminal")
+    return _query_str(power, SvPowerFlow, "Terminal")
 
 
 def voltage(voltage_vars: Iterable[Voltage] = Voltage) -> str:
-    return _query_str(voltage_vars, "SvVoltage", "TopologicalNode")
-
-
-def branch_flow(cim_version: int, power: Iterable[Power] = Power) -> str:
-    mrid = "?_mrid"
-    variables = ["?mrid", *[f"(?sv_{p}_1 as ?{p})" for p in power]]
-    branch_t = common_subject(
-        "?_t_mrid_1",
-        [
-            "rdf:type cim:Terminal",
-            f"{TC_EQUIPMENT} {mrid}",
-            f"cim:{acdc_terminal(cim_version)}.sequenceNumber 1",
-        ],
-    )
-    winding_t = f"{mrid} {TR_END}.Terminal ?_t_mrid_1"
-    where_list = [
-        f"values ?rdf_type {{{' '.join(list(LineTypes) + [TR_WINDING])}}}",
-        common_subject(mrid, [f"{ID_OBJ}.mRID ?mrid", "rdf:type ?rdf_type"]),
-        f"{{{branch_t}}} union {{{winding_t}}}",
-        *[sv_terminal_injection(1, p) for p in power],
-    ]
-    return combine_statements(select_statement(variables), group_query(where_list))
+    return _query_str(voltage_vars, SvVoltage, "TopologicalNode")
 
 
 def tapstep() -> str:
@@ -66,10 +47,11 @@ def tapstep() -> str:
     return combine_statements(select_statement(variables), group_query([where]))
 
 
-def sv_terminal_injection(nr: int, power: Power = Power.p) -> str:
+def sv_terminal_injection(nr: Optional[int] = None, power: Power = Power.p) -> str:
+    nr_str = "" if nr is None else f"_{nr}"
     return common_subject(
-        f"?sv_t_{nr}",
-        [f"{SvPowerFlow.Terminal} ?_t_mrid_{nr}", f"{SvPowerFlow.pred()}.{power} ?sv_{power}_{nr}"],
+        f"?_sv_t{nr_str}",
+        [f"{SvPowerFlow.Terminal} ?_t_mrid{nr_str}", f"{SvPowerFlow[power]} ?{power}{nr_str}"],
     )
 
 
