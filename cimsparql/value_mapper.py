4fa37a5e4938017d4a49726c31f769e252e37d3f
David Kleiven <david.kleiven@Statnett.no>
2023-03-21 09:40:47 +0000

    Add support for value mappers

---
+++ cimsparql/value_mapper.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+class ValueMapper:
+    def map(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df
+
+
+class MridMapper(ValueMapper):
+    """
+    Mapper which replaces all matches of uri_regex with an empty string
+    in columns which constains mrid in the name
+    """
+
+    def __init__(self, uri_regex: str = "^([^#_|^#]+)(#_|#)") -> None:
+        self.uri_regex = uri_regex
+
+    def map(self, df: pd.DataFrame) -> pd.DataFrame:
+        for col in filter(lambda col: "mrid" in col, df.columns):
+            df[col] = df[col].str.replace(self.uri_regex, "", regex=True)
+        return df
---
+++ tests/test_value_mapper.py
@@ -0,0 +1,51 @@
+import asyncio
+from copy import deepcopy
+from string import Template
+
+import pandas as pd
+import pytest
+import t_utils.common as t_common
+import t_utils.entsoe_models as t_entsoe
+
+from cimsparql.value_mapper import MridMapper
+
+subj_template = Template(
+    """
+prefix cim:<${cim}>
+select (?terminal as ?t_mrid) ?connected
+where {
+    ?terminal cim:ACDCTerminal.connected ?connected
+}
+"""
+)
+
+mrid_template = Template(
+    """
+prefix cim:<${cim}>
+select ?t_mrid ?connected where
+{
+    ?terminal cim:ACDCTerminal.connected ?connected;
+        cim:IdentifiedObject.mRID ?t_mrid
+}
+"""
+)
+
+
+@pytest.mark.asyncio
+async def test_subj_conversion():
+    tm = t_entsoe.micro_t1_nl_bg()
+    t_common.check_model(tm)
+    model = tm.model
+    model2 = deepcopy(model)
+    model2.config.value_mappers = [MridMapper()]
+
+    subj_query = model.template_to_query(subj_template)
+    mrid_query = model.template_to_query(mrid_template)
+
+    dfs = await asyncio.gather(
+        model2.get_table_and_convert(subj_query), model.get_table_and_convert(mrid_query)
+    )
+
+    for i, df in enumerate(dfs):
+        dfs[i] = df.sort_values("t_mrid").reset_index(drop=True)
+    pd.testing.assert_frame_equal(dfs[0], dfs[1])
