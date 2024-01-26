6cc834f762e1dd4d2b0d4d1c0244e30c0b665600
David Kleiven <david.kleiven@statnett.no>
2024-01-26 11:54:08 +0100

    test: Use micro model directly from Entsoe

---
+++ cimsparql/adaptions.py
@@ -0,0 +1,108 @@
+from __future__ import annotations
+
+import hashlib
+import re
+import uuid
+from collections.abc import Iterable
+from contextlib import suppress
+from pathlib import Path
+from string import Template
+
+from rdflib import ConjunctiveGraph
+from rdflib.namespace import XSD
+from rdflib.plugins.sparql import prepareUpdate
+from rdflib.term import Literal, URIRef
+
+
+class XmlModelAdaptor:
+    def __init__(self, filenames: Iterable[Path]) -> None:
+        self.graph = ConjunctiveGraph()
+        for filename in filenames:
+            profile = filename.stem.rpartition("/")[-1]
+            uri = URIRef(f"http://cimsparql/xml-adpator/{profile}")
+            destination_graph = self.graph.get_context(uri)
+            destination_graph.parse(filename, publicID="http://cim")
+
+    def namespaces(self) -> dict[str, str]:
+        return {str(prefix): str(name) for prefix, name in self.graph.namespaces()}
+
+    def graphs(self) -> set[URIRef]:
+        return {graph for _, _, _, graph in self.graph.quads()}
+
+    @classmethod
+    def from_folder(cls, folder: Path) -> XmlModelAdaptor:
+        return XmlModelAdaptor(list(folder.glob("*.xml")))
+
+    def add_mrid(self) -> None:
+        """
+        Adds cim:IdentifiedObject.mRID if not present
+        """
+        ns = self.namespaces()
+        identified_obj_mrid = URIRef(f"{ns['cim']}IdentifiedObject.mRID")
+        for result in self.graph.query(
+            "select ?s ?g where {graph ?g {?s cim:IdentifiedObject.name ?name}}", initNs=ns
+        ):
+            mrid_str = str(result["s"]).rpartition("#_")[-1]
+            mrid = mrid_str if is_uuid(mrid_str) else generate_uuid(mrid_str)
+            self.graph.add((result["s"], identified_obj_mrid, Literal(mrid), result["g"]))
+
+    def adapt(self) -> None:
+        self.add_zero_sv_power_flow()
+        self.add_mrid()
+        self.add_dtypes()
+
+    def add_zero_sv_power_flow(self) -> None:
+        with open(
+            Path(__file__).parent
+            / "sparql/test_configuration_modifications/add_zero_sv_power.sparql"
+        ) as f:
+            query = Template(f.read())
+
+        prepared_update_query = prepareUpdate(query.substitute(self.namespaces()))
+        self.graph.update(prepared_update_query)
+
+    def add_dtypes(self) -> None:
+        fields = {
+            "endNumber": XSD.integer,
+            "sequenceNumber": XSD.integer,
+            "phaseAngleClock": XSD.integer,
+            "SvPowerFlow.p": XSD.float,
+            ".open": XSD.boolean,
+            ".connected": XSD.boolean,
+        }
+        for s, predicate, o, g in self.graph.quads():
+            with suppress(StopIteration):
+                f = next(f for f in fields if f in predicate)
+                self.graph.remove((s, predicate, o, g))
+
+                literal = Literal(str(o), datatype=fields[f])
+                self.graph.add((s, predicate, literal, g))
+
+    def tpsvssh_contexts(self) -> list[URIRef]:
+        return [
+            ctx
+            for ctx in self.graph.contexts()
+            if any(token in str(ctx) for token in ("SSH", "TP", "SV"))
+        ]
+
+    def nq_bytes(self, contexts: list[URIRef] | None = None) -> bytes:
+        """
+        Return the contexts as bytes. If contexts is None, the entire graph
+        is exported
+        """
+        if contexts is None:
+            return self.graph.serialize(format="nquads", encoding="utf8")
+
+        graph = ConjunctiveGraph()
+        for ctx in contexts:
+            graph += self.graph.get_context(ctx.identifier)
+        return graph.serialize(format="nquads", encoding="utf8")
+
+
+def is_uuid(x: str) -> bool:
+    return re.match("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", x) is not None
+
+
+def generate_uuid(x: str) -> str:
+    h = hashlib.md5(x.encode(), usedforsecurity=False)
+    return str(uuid.UUID(hex=h.hexdigest()))
---
+++ tests/data/micro/README.md
@@ -0,0 +1,5 @@
+# Model origin
+
+Model taken from
+
+MicroGrid/Type1_T1/CGMES_v2.4.15_MicroGridTestConfiguration_T1_Assembled_Complete_v2.zip
