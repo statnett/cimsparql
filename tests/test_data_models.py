7498662f8004d54c7957887f4ff1d01027c06f10
David Kleiven <david.kleiven@statnett.no>
2024-11-18 15:04:53 +0100

    fix(test): add comment explaining test

--- tests/test_data_models.py
+++ tests/test_data_models.py
@@ -10,4 +10,6 @@ def test_empty_phase_tap_changer():
     df = pd.DataFrame([], columns=columns)
 
     validated_df = PhaseTapChangerDataFrame(df)
+
+    # Check that the target value is correctly casted from object to np.float64
     assert validated_df.dtypes.to_dict()["target_value"] == np.float64
