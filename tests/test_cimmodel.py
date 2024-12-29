e15d8cf0ee07548d589b6d62967112865d1844af
Leif Warland <leif.warland@statnett.no>
2024-12-29 18:10:20 +0100

    refactor: remove trailing comma

--- tests/test_cimmodel.py
+++ tests/test_cimmodel.py
@@ -10,17 +10,8 @@ from SPARQLWrapper import SPARQLWrapper
 
 from cimsparql.adaptions import is_uuid
 from cimsparql.graphdb import GraphDBClient, RestApi, ServiceConfig
-from cimsparql.model import (
-    Model,
-    ModelConfig,
-    get_federated_cim_model,
-)
-from cimsparql.sparql_result_json import (
-    SparqlData,
-    SparqlResultHead,
-    SparqlResultJson,
-    SparqlResultJsonFactory,
-)
+from cimsparql.model import Model, ModelConfig, get_federated_cim_model
+from cimsparql.sparql_result_json import SparqlData, SparqlResultHead, SparqlResultJson, SparqlResultJsonFactory
 from cimsparql.templates import sparql_folder
 from cimsparql.type_mapper import TypeMapper
 from cimsparql.utils import query_name
