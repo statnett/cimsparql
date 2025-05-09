55848c235e6dd35a2e3a9b34ef416cc0fbc25b7b
David Kleiven <david.kleiven@statnett.no>
2025-05-09 09:33:28 +0200

    feat: include id of equipment profile in full model query

--- tests/test_full_model.py
+++ tests/test_full_model.py
@@ -7,6 +7,7 @@ def test_full_model_1():
     model = t_custom.federated_model().model
     if not model:
         pytest.skip("Require access to GRAPPHDB")
+        return
     full_model = model.full_model()
-    assert len(full_model) == 3
-    assert full_model["time"].unique().size == 1
+    assert len(full_model) == 4
+    assert full_model[~full_model["profile"].str.contains("Equipment")]["time"].unique().size == 1
